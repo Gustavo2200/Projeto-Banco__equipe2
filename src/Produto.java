235adcad8de1df608fce0afa4d4a5cdb207a3d21
davisoles <drbp.1306@gmail.com>
2024-01-15 17:50:33 -0300

    classe Produto finalizada; ProdutoDao em processo

---
+++ src/Produto.java
@@ -0,0 +1,37 @@
+
+public class Produto {
+
+	private String nome;
+	private Integer preco;
+	private int codigoBarra;
+	private int quantidadeEstoque;
+	
+	public Produto() {}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getPreco() {
+		return preco;
+	}
+	public void setPreco(Integer preco) {
+		this.preco = preco;
+	}
+	public int getCodigoBarra() {
+		return codigoBarra;
+	}
+	public void setCodigoBarra(int codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	
+	
+}
