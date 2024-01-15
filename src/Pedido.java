d545149dcdf51fe8f411e1bb284dd01b4f25fc66
Yago Piovarczik <ypiovarczik@foursys.com.br>
2024-01-15 16:57:54 -0300

    Adicionei os get and set da classe pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -4,5 +4,30 @@ public class Pedido {
 	private Integer id;
 	private Double valorTotal;
 	private Integer quantidade;
-	
+
+	public Pedido(){}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
 }
