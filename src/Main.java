9c7cf6b9faa6cc4a749f8c79f4f89808ffb026c4
Yago Piovarczik <ypiovarczik@foursys.com.br>
2024-01-19 15:16:45 -0300

    Adicionado lista de produto

--- src/Main.java
+++ src/Main.java
@@ -7,68 +7,68 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int escolha = 0;
-		
-		while (escolha != 4) {
-			System.out.println("Olá! Digite o que voce quer fazer:\n" 
-								+"1- Adicionnar produto no estoque\n"
-								+"2- Remover produto do estoque\n"
-								+"3- Exibir lista\n"
-								+"4- Sair\n");
-			escolha = input.nextInt();
-
-			switch (escolha) {
-			case 1:
-				input.nextLine();
-				 
-				input = new Scanner(System.in);
-				
-				Produto produto = new Produto();
-
-				System.out.println("Escreva o nome do produto:\n");
-				String nomeProduto = input.nextLine();
-				
-				System.out.println("Escreva o preço do produto:\n");
-				double precoProduto = input.nextDouble();
-				
-				System.out.println("Escreva o código de barra do produto:\n");
-				int codigoProduto = input.nextInt();
-				
-				System.out.println("Qual a quantidade de produtos:\n");
-				int quantidadeProduto = input.nextInt();
-				
-				produto.setNome(nomeProduto);
-				produto.setCodigoBarra(codigoProduto);
-				produto.setPreco(precoProduto);
-				produto.setQuantidadeEstoque(quantidadeProduto);
-				
-				listaProdutos.add(produto);
-				
-				System.out.println("Produto adicionado com sucesso!");
-				
-				break;
-
-			case 2:
-				
-				System.out.println("Qual item voce quer remover");
-				int numeroRemover = input.nextInt();
-				listaProdutos.remove(numeroRemover - 1);
-				
-				break;
-
-			case 3:
-
-				for (int i = 0; i < listaDeTarefas.size(); i++) {
-					System.out.println((i + 1) + "- " + listaDeTarefas.get(i).getNome() +"\n" + listaDeTarefas.get(i).getDescrição()+ ";" + "\n");
-				}
-
-				break;
-			case 4:
-				System.out.println("Programa finalizado");
-			}
-
-		}
-
-	}
+//		int escolha = 0;
+//		
+//		while (escolha != 4) {
+//			System.out.println("Olá! Digite o que voce quer fazer:\n" 
+//								+"1- Adicionnar produto no estoque\n"
+//								+"2- Remover produto do estoque\n"
+//								+"3- Exibir lista\n"
+//								+"4- Sair\n")
+////			escolha = input.nextInt();
+//
+//			switch (escolha) {
+//			case 1:
+//				input.nextLine();
+//				 
+//				input = new Scanner(System.in);
+//				
+//				Produto produto = new Produto();
+//
+//				System.out.println("Escreva o nome do produto:\n");
+//				String nomeProduto = input.nextLine();
+//				
+//				System.out.println("Escreva o preço do produto:\n");
+//				double precoProduto = input.nextDouble();
+//				
+//				System.out.println("Escreva o código de barra do produto:\n");
+//				int codigoProduto = input.nextInt();
+//				
+//				System.out.println("Qual a quantidade de produtos:\n");
+//				int quantidadeProduto = input.nextInt();
+//				
+//				produto.setNome(nomeProduto);
+//				produto.setCodigoBarra(codigoProduto);
+//				produto.setPreco(precoProduto);
+//				produto.setQuantidadeEstoque(quantidadeProduto);
+//				
+//				listaProdutos.add(produto);
+//				
+//				System.out.println("Produto adicionado com sucesso!");
+//				
+//				break;
+//
+//			case 2:
+//				
+//				System.out.println("Qual item voce quer remover");
+//				int numeroRemover = input.nextInt();
+//				listaProdutos.remove(numeroRemover - 1);
+//				
+//				break;
+//
+//			case 3:
+//
+//				for (int i = 0; i < listaDeTarefas.size(); i++) {
+//					System.out.println((i + 1) + "- " + listaDeTarefas.get(i).getNome() +"\n" + listaDeTarefas.get(i).getDescrição()+ ";" + "\n");
+//				}
+//
+//				break;
+//			case 4:
+//				System.out.println("Programa finalizado");
+//			}
+//
+//		}
+//
+  }
 
 }
--- src/br/com/cdb/dao/ProdutoDao.java
+++ src/br/com/cdb/dao/ProdutoDao.java
@@ -1,24 +1,37 @@
 package br.com.cdb.dao;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import br.com.cbd.model.Produto;
 
 public class ProdutoDao {
-	
-	ArrayList<Produto> listaProdutos= new ArrayList<>();
-	
-	public boolean adicionarProduto(Produto produto) 
-	{
+
+	ArrayList<Produto> listaProdutos = new ArrayList<>();
+
+	public boolean adicionarProduto(Produto produto) {
 		listaProdutos.add(produto);
 		return true;
 	}
-	
-	public boolean retirarProduto(Produto produto) 
-	{
+
+	public boolean retirarProduto(Produto produto) {
 		listaProdutos.remove(0);
 		return true;
 	}
-	
+
+    public ArrayList<Produto> listaProduto () {
+	return listaProdutos;
+  }
+    
+    public Produto buscarProduto(int codigoBarra) {
+    	for( Produto produto : listaProdutos ) {
+    		{
+    			if( produto.getCodigoBarra() == codigoBarra) 
+    			return produto;
+    		}
+    		
+    	}
+		return null;
+    }
 
 }
