1edf5cec1282fb125f7258557c70ac15eb7062f9
davisoles <drbp.1306@gmail.com>
2024-01-24 15:32:15 -0300

    adicionado Produto Service

--- src/br/com/cdb/service/ProdutoService.java
+++ src/br/com/cdb/service/ProdutoService.java
@@ -1,35 +1,59 @@
 package br.com.cdb.service;
 
+import java.util.ArrayList;
+
 import br.com.cdb.dao.ProdutoDao;
 import br.com.cdb.model.Produto;
 
 public class ProdutoService {
-	
+
 	ProdutoDao produtoDao = new ProdutoDao();
-	
+
 	public boolean verificarProdutoExiste(int codigoBarra) {
-		
-		for (Produto produto : produtoDao.listaProduto() ) {
-			
+
+		for (Produto produto : produtoDao.listaProduto()) {
+
 			if (produto.getCodigoBarra() == codigoBarra) {
 				return true;
 			}
-			
+
 		}
 		return false;
-		
+
 	}
-	
+
 	public boolean verificarTamanhoCodigo(int codigoBarra) {
-		
+
 		if (Integer.toString(codigoBarra).length() == 7) {
 			return true;
 		}
 		return false;
-		
-		
 	}
 
+	public boolean adicionarProduto(Produto produto) {
+		Produto produto1 = new Produto();
+		
+		produto1.setNome(produto.getNome());
+		produto1.setCodigoBarra(produto.getCodigoBarra());
+		produto1.setPreco(produto.getPreco());
+		produto1.setQuantidadeEstoque(produto.getQuantidadeEstoque());
+		
+		if (verificarTamanhoCodigo(produto1.getCodigoBarra())) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean retirarProduto(Produto produto) {	
+		return produtoDao.retirarProduto(produto);
+	}
+	
+	public ArrayList<Produto> listaProduto (){
+		return produtoDao.listaProduto();
+	}
+	
+	 public Produto buscarProduto(int codigoBarra) {
+		 return produtoDao.buscarProduto(codigoBarra);
+	 }
+
 }
-
-
